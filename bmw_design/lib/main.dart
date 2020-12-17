f6283c074d7e3cf7ad5a11f7b51d59ce7f7f20c7
Germán Insua <1996GInsua@gmail.com>
2020-12-17 13:00:07 +0000

    Added cloudfirebase auth and login screen sing in/up
    Co-Authored-By: polcamacho <47743832+polcamacho@users.noreply.github.com>

--- bmw_design/lib/main.dart
+++ bmw_design/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'app.dart';
+import 'screens/MyApp.dart';
+import 'sign_in_flow/auth_state_switch.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(CarInfoApp());
+  runApp(
+    AuthStateSwitch(
+      app: LoginScreen(),
+    ),
+  );
 }
